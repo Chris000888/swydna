1c7f0bfd137940472814dffa1878a7d3a63436e1
Chris000888 <christopherdato08@gmail.com>
2020-06-15 15:17:07 +0000

    Wit.ai connection

--- fbtestbot.py
+++ fbtestbot.py
@@ -40,7 +40,7 @@ def handle_messages():
                         send_message(sender_id, task)
                         buttons = show_buttons()
                         log(buttons)
-                        bot.send_generic_message(sender_id, buttons)
+                        bot.send_generic_message(sender_id, buttons['buttons'])
                     elif entity == 'asking':
                         task = 'What do you want?'
                         send_message(sender_id, task)
