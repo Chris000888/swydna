9db1a2783ae131f22931cf582e0f813022f00617
Chris000888 <christopherdato08@gmail.com>
2020-06-15 14:55:45 +0000

    Wit.ai connection

--- utils.py
+++ utils.py
@@ -19,18 +19,26 @@ def wit_response(message_text):
 def show_buttons():
 
     buttons = {
-        "buttons":[
-          {
-            "type":"web_url",
-            "url":"https://www.messenger.com",
-            "title":"Visit Messenger1"
-          },
-          {
-            "type":"web_url",
-            "url":"https://www.messenger.com",
-            "title":"Visit Messenger2"
-          }
-        ]
+        "message":{
+            "attachment":{
+                "type":"template",
+                "payload":{
+                    "template_type":"button",
+                    "text":"What do you want to do next?",
+                    "buttons":[
+                        {
+                            "type":"web_url",
+                            "url":"https://www.messenger.com",
+                            "title":"Visit Messenger"
+                        },
+                        {
+                            ...
+                        },
+                        {...}
+                    ]
+                }
+            }
+        }
     }
                 
      
